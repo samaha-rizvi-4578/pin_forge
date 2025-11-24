9824c419cec88b237ad2e0f455a418df0859edd2
Syeda Samaha Batool Rizvi <94197721+samaha-rizvi-4578@users.noreply.github.com>
2025-11-24 15:06:54 +0500

    Update urls.py

--- pin_forge/pin_automate/urls.py
+++ pin_forge/pin_automate/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
-
+from . import views
 from .views import (
     UserViewSet,
     GroupViewSet,
     StoreViewSet,
     ProductViewSet,
+    PinTemplateViewSet,
+    GeneratedPinViewSet
 )
 
 # -----------------------------
@@ -22,10 +24,17 @@ router.register(r'stores', StoreViewSet, basename='store')
 
 # Products (with nested variants)
 router.register(r'products', ProductViewSet, basename='product')
+# pintempaltes
+router.register(r'pintemplates', PinTemplateViewSet, basename='pintemplate')
+# generatedpin
+router.register(r'generatedpins', GeneratedPinViewSet, basename='generatedpin')
+
 
 # -----------------------------
 # URL PATTERNS
 # -----------------------------
 urlpatterns = [
+    path("pinterest/start/", views.pinterest_auth_start, name="pinterest_start"),
+    path("pinterest/callback/", views.pinterest_auth_callback, name="pinterest_callback"),
     path('', include(router.urls)),
 ]
