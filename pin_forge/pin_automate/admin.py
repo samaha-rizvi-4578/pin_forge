071927adf28cb8436d208be11ae1b952395b5945
Syeda Samaha Batool Rizvi <94197721+samaha-rizvi-4578@users.noreply.github.com>
2025-11-24 15:05:31 +0500

    Update admin.py

--- pin_forge/pin_automate/admin.py
+++ pin_forge/pin_automate/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Store, Product, Variant
+from .models import Store, Product, Variant, PinTemplate, GeneratedPin
 admin.site.register(Store)
 admin.site.register(Product)
 admin.site.register(Variant)
+admin.site.register(PinTemplate)
+admin.site.register(GeneratedPin)
 
